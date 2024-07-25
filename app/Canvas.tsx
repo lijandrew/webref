a1aef6d0cc6249ff27c3d0cfde671ba87deed85e
Andrew Li <li.jj.andrew@gmail.com>
2024-07-25 17:08:11 -0400

    remove and put canvas panning on backburner for now

--- app/Canvas.tsx
+++ app/Canvas.tsx
@@ -5,5 +5,5 @@ import { Rnd } from "react-rnd";
 
 export default function Canvas() {
   const refMap = useStore((state) => state.refMap);
-  return <Rnd className={styles.Canvas}> {Array.from(refMap.values())} </Rnd>;
+  return <div className={styles.Canvas}> {Array.from(refMap.values())} </div>;
 }
